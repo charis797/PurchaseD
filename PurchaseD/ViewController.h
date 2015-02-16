80c9cdc70fd186a1cf27253e09a760fa2417ffd4
Office <office@Rajeshs-MacBook-Pro.local>
2015-02-16 17:41:48 +0530

    Initial Commit

---
+++ PurchaseD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PurchaseD
+//
+//  Created by Office on 16/02/15.
+//  Copyright (c) 2015 Charis Communication. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PurchaseDTests/PurchaseDTests.m
@@ -0,0 +1,40 @@
+//
+//  PurchaseDTests.m
+//  PurchaseDTests
+//
+//  Created by Office on 16/02/15.
+//  Copyright (c) 2015 Charis Communication. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PurchaseDTests : XCTestCase
+
+@end
+
+@implementation PurchaseDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
